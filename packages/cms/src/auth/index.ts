ec1c25e2b4f665e5e457125a2b2378c3c8cc7630
Forrest Blade <mail@forrestblade.com>
2026-03-19 23:53:05 -0500

    GREEN: implement destroyUserSessions and call before createSession in login

--- packages/cms/src/auth/index.ts
+++ packages/cms/src/auth/index.ts
@@ -3,7 +3,7 @@ export type { AuthConfig } from './auth-config.js'
 
 export { hashPassword, verifyPassword } from './password.js'
 
-export { createSession, validateSession, destroySession, buildSessionCookie, buildExpiredSessionCookie } from './session.js'
+export { createSession, validateSession, destroySession, destroyUserSessions, buildSessionCookie, buildExpiredSessionCookie } from './session.js'
 
 export { createAuthMiddleware } from './middleware.js'
 export type { AuthContext, AuthMiddleware } from './middleware.js'
